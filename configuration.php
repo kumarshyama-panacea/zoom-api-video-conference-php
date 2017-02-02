eb67377029067aa3204aea21503f7965b26493c0
kumarshyama-panacea <shyama@panaceatek.com>
2017-02-02 16:20:56 +0530

    Update configuration.php

--- configuration.php
+++ configuration.php
@@ -3,9 +3,9 @@
  * Here all common required details related to account will be stored
  */
 
-define('API_KEY', '9XQO35Q5T9y11MEPSkU0dA');
+define('API_KEY', '');
 
-define('API_SECRET', '14wuOTSw3qB4OpTjxzhuGCYhZdJr0FvxSxOr');
+define('API_SECRET', '');
 
 define('API_URL', 'https://api.zoom.us/v1/');
 
